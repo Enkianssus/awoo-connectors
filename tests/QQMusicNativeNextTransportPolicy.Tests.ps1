30731260510349aa152eb421032e7bc78159df40
Enkianssus <69905090+Enkianssus@users.noreply.github.com>
2026-08-27 14:35:59 +0800

    use detected x86 runtime in release smoke

--- tests/QQMusicNativeNextTransportPolicy.Tests.ps1
+++ tests/QQMusicNativeNextTransportPolicy.Tests.ps1
@@ -63,7 +63,7 @@ foreach ($expectedProfile in $expectedProfiles) {
     }
 }
 
-if ([string]$project.Project.PropertyGroup.Version -ne '22.52.3') {
+if ([string]$project.Project.PropertyGroup.Version -ne '22.52.4') {
     throw 'QQ Music connector version must follow the tested QQ Music 22.52 branch.'
 }
 
